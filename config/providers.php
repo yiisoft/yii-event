aa57c685615fdb3634c39bc367f7cdc7feffc063
Wilmer Arambula <terabytesoftw@gmail.com>
2020-11-10 13:10:14 -0300

    Enhacements.

---
+++ config/providers.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Yiisoft\Arrays\Modifier\ReverseBlockMerge;
+use Yiisoft\Composer\Config\Builder;
+use Yiisoft\Yii\Event\EventDispatcherProvider;
+
+return [
+    'yiisoft/event-dispatcher/eventdispatcher' => [
+        '__class' => EventDispatcherProvider::class,
+        '__construct()' => [Builder::require('events')]
+    ],
+
+    ReverseBlockMerge::class => new ReverseBlockMerge(),
+];
