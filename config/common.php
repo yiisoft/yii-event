ae42317bef2cfe3dba52dba7697d9a7b8a003c89
Alexander Makarov <sam@rmcreative.ru>
2020-11-16 01:17:08 +0300

    Add missing imports

--- config/common.php
+++ config/common.php
@@ -2,8 +2,12 @@
 
 declare(strict_types=1);
 
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\EventDispatcher\ListenerProviderInterface;
 use Yiisoft\Composer\Config\Builder;
 use Yiisoft\EventDispatcher\Provider\ListenerCollection;
+use Yiisoft\EventDispatcher\Provider\Provider;
+use Yiisoft\Factory\Definitions\Reference;
 use Yiisoft\Yii\Event\ListenerCollectionFactory as Factory;
 
 return [
