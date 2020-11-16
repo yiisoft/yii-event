ace3e9854024a2b38ece42fba21c314bc11c3357
Alexander Makarov <sam@rmcreative.ru>
2020-11-16 18:58:22 +0300

    Fix Dispatcher namespace

--- config/common.php
+++ config/common.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Psr\EventDispatcher\ListenerProviderInterface;
 use Yiisoft\Composer\Config\Builder;
+use Yiisoft\EventDispatcher\Dispatcher\Dispatcher;
 use Yiisoft\EventDispatcher\Provider\ListenerCollection;
 use Yiisoft\EventDispatcher\Provider\Provider;
 use Yiisoft\Yii\Event\ListenerCollectionFactory as Factory;
 
 return [
     ListenerCollection::class => static fn (Factory $factory) => $factory->create(require Builder::path('events')),
-    EventDispatcherInterface::class => Yiisoft\EventDispatcher\Dispatcher\Dispatcher::class,
+    EventDispatcherInterface::class => Dispatcher::class,
     ListenerProviderInterface::class => Provider::class,
 ];
