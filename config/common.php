b9b3a008f6ad99d3511b6b7cc251e743207bb758
viktorprogger <viktorprogger@gmail.com>
2020-11-16 15:15:26 +0300

    Simplify config

--- config/common.php
+++ config/common.php
@@ -7,14 +7,10 @@ use Psr\EventDispatcher\ListenerProviderInterface;
 use Yiisoft\Composer\Config\Builder;
 use Yiisoft\EventDispatcher\Provider\ListenerCollection;
 use Yiisoft\EventDispatcher\Provider\Provider;
-use Yiisoft\Factory\Definitions\Reference;
 use Yiisoft\Yii\Event\ListenerCollectionFactory as Factory;
 
 return [
     ListenerCollection::class => static fn (Factory $factory) => $factory->create(require Builder::path('events')),
     EventDispatcherInterface::class => Yiisoft\EventDispatcher\Dispatcher\Dispatcher::class,
-    ListenerProviderInterface::class => [
-        '__class' => Provider::class,
-        '__construct()' => [Reference::to(ListenerCollection::class)]
-    ]
+    ListenerProviderInterface::class => Provider::class,
 ];
