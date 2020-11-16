8cc5e7d35fac90d341391afa19f74c8df84d0793
Rustam Mamadaminov <rmamdaminov@gmail.com>
2020-11-16 21:22:47 +0500

    Fix config (#12)
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- config/common.php
+++ config/common.php
@@ -6,12 +6,9 @@ use Psr\EventDispatcher\EventDispatcherInterface;
 use Psr\EventDispatcher\ListenerProviderInterface;
 use Yiisoft\Composer\Config\Builder;
 use Yiisoft\EventDispatcher\Dispatcher\Dispatcher;
-use Yiisoft\EventDispatcher\Provider\ListenerCollection;
 use Yiisoft\EventDispatcher\Provider\Provider;
-use Yiisoft\Yii\Event\ListenerCollectionFactory as Factory;
 
 return [
-    ListenerCollection::class => static fn (Factory $factory) => $factory->create(require Builder::path('events')),
     EventDispatcherInterface::class => Dispatcher::class,
     ListenerProviderInterface::class => Provider::class,
 ];
