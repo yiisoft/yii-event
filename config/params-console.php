436cf816409ce1af87203bbb748270b1d81ae3cd
Alexander Makarov <sam@rmcreative.ru>
2024-09-13 12:41:48 +0300

    Add debug command

--- config/params-console.php
+++ config/params-console.php
@@ -2,8 +2,18 @@
 
 declare(strict_types=1);
 
+use Yiisoft\Yii\Event\Command\DebugEventsCommand;
+
 return [
     'yiisoft/yii-event' => [
         'eventsConfigGroup' => 'events-console',
     ],
+    'yiisoft/yii-debug' => [
+        'ignoredCommands' => [
+            'debug:events',
+        ],
+    ],
+    'yiisoft/yii-console' => [
+        'debug:events' => DebugEventsCommand::class,
+    ]
 ];
